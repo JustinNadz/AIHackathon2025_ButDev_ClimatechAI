c6831204639753fb9891ad32928bd888b7fff386
sininen-blue <alaanneil@gmail.com>
2025-08-11 16:47:20 +0800

    feat: vectore store functionality

---
+++ backend/vectordb/store.py
@@ -0,0 +1,19 @@
+import os
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from config import OPENROUTER_API_KEY, CHROMA_DB_DIR
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-large",
+    openai_api_key=OPENROUTER_API_KEY,
+    openai_api_base="https://openrouter.ai/api/v1"
+)
+
+
+def get_vectorstore():
+    chroma = Chroma(
+        persist_directory=CHROMA_DB_DIR,
+        embedding_function=embeddings,
+    )
+
+    return chroma
