6f1c2426bbdb724b3cbfd7f19cf318fa0420eaeb
KENT ADRIAN P. SABAYDAY <kentadrianpalaciosabayday@gmail.com>
2025-08-12 20:53:55 +0800

    new ui with connection to backend

--- PivotFrontend/src/App.js
+++ PivotFrontend/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import MapComponent from './components/MapComponent';
 import AISummaryPanel from './components/AISummaryPanel';
+import ChatBot from './components/ChatBot';
 
 function App() {
   return (
     <div className="App">
       <div className="split-container">
+        <div className="chatbot-section">
+          <ChatBot />
+        </div>
         <div className="map-section">
           <MapComponent />
         </div>
